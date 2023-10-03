851d33628703ae384a506d3f31bb85ba2612b1e7
kostiaxn <kostia.xn@gmail.com>
2023-10-03 15:26:22 +0300

    Java для начинающих. Урок 2: Переменные. Примитивные типы данных.

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte myByte = 12;
+        int myInt = 10;
+        short myShort = 1235;
+        double myDouble = 12.44;
+        float myFloat = 12.54f;
+        long myLong = 1544354519;
+        char myChar = 'A';
+        boolean myBoolean = true;
+
+    }
+}
