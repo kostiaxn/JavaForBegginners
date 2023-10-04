1ed6121d00c9d97bdb3b4978a36f13abd211b166
kostiaxn <kostia.xn@gmail.com>
2023-10-04 11:52:44 +0300

    Урок 10: Оператор switch.

---
+++ src/Switch.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи возраст");
+        int age = scanner.nextInt();
+        switch (age) {
+            case 0:
+                System.out.println("ты родился");
+                break;
+            case 7:
+                System.out.println("ты пошел в школу");
+                break;
+            case 18:
+                System.out.println("ты закончил школу");
+                break;
+            default:
+                System.out.println("ни одно из предыдущих условий не подошло");
+        }
+    }
+}
