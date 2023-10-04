b45012c241cb262be707f278e6a6d6d3a75c282d
kostiaxn <kostia.xn@gmail.com>
2023-10-04 10:37:21 +0300

    Урок 9: Операторы break и continue

---
+++ src/Break_Continue.java
@@ -0,0 +1,18 @@
+public class Break_Continue {
+    public static void main(String[] args) {
+        int value = 0;
+        while (true) {
+            System.out.println(value);
+            value++;
+            if (value == 5) {
+                break;
+            }
+        }
+        for (int i = 0; i <= 15; i++) {
+            if (i % 2 == 1) {
+                continue;
+            }
+            System.out.println("Четное число " + i);
+        }
+    }
+}
