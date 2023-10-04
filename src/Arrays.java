1031fb6fa852bc920f391d53343934459277755e
kostiaxn <kostia.xn@gmail.com>
2023-10-04 13:07:43 +0300

    Вне курса по Java. Калькулятор для вычисления разницы при разных курсах валют.

---
+++ src/Arrays.java
@@ -0,0 +1,7 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int number = 10; // примитивный тип данных
+        char character = 'a'; // примитивный тип данных
+
+    }
+}
---
+++ src/Currency.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Currency {
+
+    public static void main(String[] args) {
+        double amountBYN;
+        double firstCurrencyUSD;
+        double secondCurrencyUSD;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первый курс в USD");
+        firstCurrencyUSD = scanner.nextDouble();
+
+        System.out.println("Введите второй курс в USD");
+        secondCurrencyUSD = scanner.nextDouble();
+        System.out.println("Введите сумму в BYN для конвертации");
+        amountBYN = scanner.nextDouble();
+
+        double amountUSD1 = amountBYN / firstCurrencyUSD;
+        double amountUSD2 = amountBYN / secondCurrencyUSD;
+
+        System.out.println("Сумма USD по первому курсу: " + Math.round(amountUSD1) + " USD");
+        System.out.println("Сумма USD по второму курсу: " + Math.round(amountUSD2) + " USD");
+        System.out.println("Разница между курсами USD 1 - USD 2: " + Math.round(amountUSD1 - amountUSD2) + " USD");
+
+
+    }
+
+
+}
