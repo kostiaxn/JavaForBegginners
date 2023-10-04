4be76f990593d3484192fa81fd6f167c186c8264
kostiaxn <kostia.xn@gmail.com>
2023-10-04 14:34:32 +0300

    Урок 11: Массивы в Java.

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,7 +1,12 @@
 public class Arrays {
     public static void main(String[] args) {
         int number = 10; // примитивный тип данных
-        char character = 'a'; // примитивный тип данных
-
+        int[] numbers = new int[100]; // ссылочный тип данных
+        for (int i = 0; i < numbers.length ; i++) {
+            numbers[i] = i*10;
+        }
+        for (int i = 0; i < numbers.length ; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 }
